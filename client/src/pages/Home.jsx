e311e803a3206a8cf258357612d87194a914b4c7
mani chegu <chegu.mani2020@gmail.com>
2023-11-24 22:31:05 +0530

    commit the changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -191,7 +191,7 @@ export default function Home() {
           place with ease
         </h1>
         <div className="text-muted fs-6">
-          Estate Xplorer is the best place to find your next perfect place to
+          Estate Xplorer the best place to find your next perfect place to
           live.
           <br />
           We have a wide range of properties for you to choose from.
